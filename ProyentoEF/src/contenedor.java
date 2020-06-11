66d9c09d6164437e42620f84c57de74eea9d37be
carlosalbertoflores <carlos.depaz1411@gmail.com>
2020-06-11 09:37:13 -0600

    contenedor

--- ProyentoEF/src/contenedor.java
+++ ProyentoEF/src/contenedor.java
@@ -32,11 +32,11 @@ public class contenedor extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 473, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 282, Short.MAX_VALUE)
         );
 
         pack();
